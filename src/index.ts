fc69815a403a4fe7ee6124fda95523e04a0f0047
victorhdsp <victorhugods.pereira@gmail.com>
2025-08-22 20:01:54 -0300

    refactor: :recycle: split lexer logic

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import { OnoperIntermediary } from "./intermediary/intermediary";
+import { OnoperCommonAnalysis } from "./lexicalAnalysis/commonMetacaracteres";
+import { OnoperConfigAnalysis } from "./lexicalAnalysis/configMetacaracteres";
 import { OnoperLexicalAnalysis } from "./lexicalAnalysis/lexicalAnalysis";
 import { OnoperRenderer } from "./renderer/renderer";
 import { OnoperSyntacticAnalysis } from "./syntacticAnalysis/syntacticAnalysis";
 
 export class Onoper {
     execute(content: string): string {
-        const syntacticAnalysis = new OnoperSyntacticAnalysis();
-        const lexer = new OnoperLexicalAnalysis();
-        const intermediary = new OnoperIntermediary();
-        const renderer = new OnoperRenderer();
-
+        const configAnalysis = new OnoperConfigAnalysis();
+        const commonAnalysis = new OnoperCommonAnalysis();
+        const lexer = new OnoperLexicalAnalysis(configAnalysis, commonAnalysis);
         const lexedList = lexer.execute(content);
-        
+
+        const syntacticAnalysis = new OnoperSyntacticAnalysis();
         syntacticAnalysis.execute(lexedList);
 
+        const intermediary = new OnoperIntermediary();
         const intermediaryResult = intermediary.execute(lexedList);
 
+        const renderer = new OnoperRenderer();
         return renderer.render(intermediaryResult);
     }
 }
